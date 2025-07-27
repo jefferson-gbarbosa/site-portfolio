2cf2496613949f23704709610228b9ce7d741db6
jefferson-gbarbosa <jeffersonqx@gmail.com>
2025-07-27 19:49:12 -0300

    :tada: initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss(), svgr()]
+  }
+});
