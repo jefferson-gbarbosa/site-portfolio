183ac707f95533fdc222a67f7d95b5c9fdde7a7a
jefferson-gbarbosa <jeffersonqx@gmail.com>
2025-08-04 16:04:34 -0300

    :boom: fix: Reverting efficient settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-import svgr from 'vite-plugin-svgr';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss(), svgr()]
-  }
+  integrations: [tailwind(), sitemap()],
 });
