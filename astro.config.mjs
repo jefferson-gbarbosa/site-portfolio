28a3d8f5662d0171938b4828f94d4f9bc23ee8bc
jefferson-gbarbosa <jeffersonqx@gmail.com>
2025-08-04 23:36:43 -0300

    :chore(seo): add robots.txt to allow search engine indexing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://portfolio-jefferson-dev.netlify.app/',
   integrations: [tailwind(), sitemap()],
 });
